c433997e63a659f7c599ee72e140d2063ac097e5
Jocseh07 <josephmkaranja07@gmail.com>
2024-08-20 07:56:40 +0300

    add task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -21,7 +21,7 @@ def print_stats(total_size, status_codes):
     print("File size:", total_size)
     for code in status_codes.keys():
         if status_codes[code] > 0:
-            print(code, ":", status_codes[code])
+            print(f"{code}: {status_codes[code]}")
 
 
 def parse_line(line):
