7cb2b19289158c3945b5e461b94a4cdad3ff0508
Jocseh07 <josephmkaranja07@gmail.com>
2024-09-05 09:52:45 +0300

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,21 +6,33 @@ const url = 'https://swapi-api.alx-tools.com/api/films/';
 
 const characterId = process.argv[2];
 
-request(url + characterId, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    const sortedCharacters = characters.sort((a, b) => a - b);
-    console.log(sortedCharacters);
-    for (const character of sortedCharacters) {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+// request(url + characterId, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     const characters = JSON.parse(body).characters;
+//     const sortedCharacters = characters.sort((a, b) => a - b);
+//     console.log(sortedCharacters);
+//     for (const character of sortedCharacters) {
+//       request(character, (error, response, body) => {
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log(JSON.parse(body).name);
+//         }
+//       });
+//     }
+//   }
+// });
+
+const getMovie = async (url) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      return error;
+    } else {
+      return JSON.parse(body);
     }
-  }
-});
+  });
+};
+
+console.log(getMovie(url));
