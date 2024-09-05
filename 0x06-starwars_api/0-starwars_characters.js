fba2ae4af06425843ea5f9db93aba6aaaf632d8b
Jocseh07 <josephmkaranja07@gmail.com>
2024-09-05 10:22:59 +0300

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,19 +4,7 @@ const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films';
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-const movieId = process.argv[2];
-if (movieId < 1 || movieId > 7) {
-  console.log('Movie id must be between 1 and 7');
-  process.exit(1);
-}
-
-const movieUrl = `${url}/${movieId}`;
-
-request(movieUrl, async (error, response, body) => {
+request(`${url}/${movieId}`, async (error, response, body) => {
   if (error) throw error;
   const movieCharacters = JSON.parse(body).characters;
   getCharacterNames(movieCharacters, 0);
