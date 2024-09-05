3250cc3bd3405fb43ff14e06b4393f8ffc20890b
Jocseh07 <josephmkaranja07@gmail.com>
2024-09-05 10:06:42 +0300

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,37 +2,30 @@
 
 import request from 'request';
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const movieId = process.argv[2];
 
-const characterId = process.argv[2];
+const movieUrl = `${url}/${movieId}`;
 
-// request(url + characterId, (error, response, body) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     const characters = JSON.parse(body).characters;
-//     const sortedCharacters = characters.sort((a, b) => a - b);
-//     console.log(sortedCharacters);
-//     for (const character of sortedCharacters) {
-//       request(character, (error, response, body) => {
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.log(JSON.parse(body).name);
-//         }
-//       });
-//     }
-//   }
-// });
+request(movieUrl, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
 
-const getMovie = async (url) => {
-  request(url, (error, response, body) => {
-    if (error) {
-      return error;
-    } else {
-      return JSON.parse(body);
+    for (const character of characters) {
+      const characterData = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body));
+          }
+        });
+      });
+
+      console.log(characterData.name);
     }
-  });
-};
-
-console.log(getMovie(url));
+  }
+});
