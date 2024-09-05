4cdf796429192a1d2916831181f4b9ee65910839
Jocseh07 <josephmkaranja07@gmail.com>
2024-09-05 09:50:33 +0300

    all

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+import request from 'request';
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+const characterId = process.argv[2];
+
+request(url + characterId, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    const sortedCharacters = characters.sort((a, b) => a - b);
+    console.log(sortedCharacters);
+    for (const character of sortedCharacters) {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
