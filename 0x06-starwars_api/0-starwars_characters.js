89908d74d58a1b4a263b5e19b25f5f83aae6eb76
Jocseh07 <josephmkaranja07@gmail.com>
2024-09-05 10:25:56 +0300

    all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -22,11 +22,11 @@ request(movieUrl, (error, response, body) => {
   getCharacterNames(movieCharacters, 0);
 });
 
-function getCharacterNames(movieCharacters, index) {
+const getCharacterNames = (movieCharacters, index) => {
   if (index === movieCharacters.length) return;
   request(movieCharacters[index], (error, response, body) => {
     if (error) throw error;
     console.log(JSON.parse(body).name);
     getCharacterNames(movieCharacters, index + 1);
   });
-}
+};
