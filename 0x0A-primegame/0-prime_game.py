f3c3a491d1d00670b6542a554124c79aa990d300
Jocseh07 <josephmkaranja07@gmail.com>
2024-10-03 08:29:13 +0300

    task 0

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def getPrimes(n):
+    """Get all prime numbers less than n"""
+    primes = []
+    for i in range(2, n + 1):
+        isPrime = True
+        for j in range(2, i):
+            if i % j == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(i)
+    return primes
+
+
+def isWinner(x, nums):
+    """Prime Game"""
+    Maria = 0
+    Ben = 0
+    for n in nums:
+        primes = getPrimes(n)
+        if len(primes) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return "Maria"
+    return "Ben"
