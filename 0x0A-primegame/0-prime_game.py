76f0a364d5d1718d968ebc8fd4284a27dd36673b
Jocseh07 <josephmkaranja07@gmail.com>
2024-10-03 08:59:12 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,6 @@ def getPrimes(n):
     for i in range(2, n + 1):
         isPrime = True
         for j in primes:
-            # for j in range(2, i):
             if i % j == 0:
                 isPrime = False
                 break
@@ -23,20 +22,16 @@ def isWinner(x, nums):
         return None
     if x != len(nums):
         return None
+
     Maria = 0
     Ben = 0
-    rounds = 0
+
     for n in nums:
-        # if rounds == x:
-        #     break
-        # round += 1
         primes = getPrimes(n)
         if len(primes) % 2 == 0:
             Ben += 1
-            print("Ben")
         else:
             Maria += 1
-            print("Maria")
     if Maria > Ben:
         return "Maria"
     return "Ben"
