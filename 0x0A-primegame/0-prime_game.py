5d1b82af6a2973664f9e33d6615ed6c7783576cc
Jocseh07 <josephmkaranja07@gmail.com>
2024-10-03 08:34:17 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,7 +7,7 @@ def getPrimes(n):
     primes = []
     for i in range(2, n + 1):
         isPrime = True
-        for j in range(2, i):
+        for j in primes:
             if i % j == 0:
                 isPrime = False
                 break
