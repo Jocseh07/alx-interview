dbb945210b5abfe242691a6252b98c7d08b36f2e
Jocseh07 <josephmkaranja07@gmail.com>
2024-10-06 19:17:32 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,7 +7,7 @@ def sieve(n):
     is_prime = [True] * (n + 1)
     p = 2
     while (p * p <= n):
-        if (is_prime[p] == True):
+        if (is_prime[p]):
             for i in range(p * p, n + 1, p):
                 is_prime[i] = False
         p += 1
