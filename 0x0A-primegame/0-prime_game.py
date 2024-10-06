dfa950a47e0d0eb4f966934a70bc76355d21fc9f
Jocseh07 <josephmkaranja07@gmail.com>
2024-10-06 19:14:02 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,37 +2,40 @@
 """Prime Game"""
 
 
-def getPrimes(n):
-    """Get all prime numbers less than n"""
-    primes = []
-    for i in range(2, n + 1):
-        isPrime = True
-        for j in primes:
-            if i % j == 0:
-                isPrime = False
-                break
-        if isPrime:
-            primes.append(i)
+def sieve(n):
+    """Generate a list of primes up to n using the Sieve of Eratosthenes"""
+    is_prime = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if (is_prime[p] == True):
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+        p += 1
+    primes = [p for p in range(2, n + 1) if is_prime[p]]
     return primes
 
 
 def isWinner(x, nums):
     """Prime Game"""
-    # if x < 1 or not nums:
-    #     return None
     if x != len(nums):
         return None
+
+    max_n = max(nums)
+    primes = sieve(max_n)
 
     Maria = 0
     Ben = 0
 
     for n in nums:
-        primes = getPrimes(n)
-
-        if len(primes) % 2 == 0:
+        current_primes = [p for p in primes if p <= n]
+        if len(current_primes) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
+
     if Maria > Ben:
         return "Maria"
-    return "Ben"
+    elif Ben > Maria:
+        return "Ben"
+    else:
+        return None
